da4e9f9bd3599ff94de5ad0b40b98d12e65d3935
Shlomi Nugarker <shlomin1231@gmail.com>
2025-12-12 12:00:58 +0200

    feat: Implement Todo app with task management features

--- lib/main.dart
+++ lib/main.dart
@@ -1,120 +1,173 @@
 import 'package:flutter/material.dart';
 
+// Entry point of the application
 void main() {
   runApp(const MyApp());
 }
 
+// ========================================
+// Data Model - represents a single task
+// ========================================
+class Todo {
+  String title; // Task text
+  bool isDone; // Is completed
+
+  Todo({required this.title, this.isDone = false});
+}
+
+// ========================================
+// Main Widget - App configuration
+// ========================================
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Todo App',
+      debugShowCheckedModeBanner: false, // Hides the "DEBUG" banner
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TodoApp(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+// ========================================
+// TodoApp - Main screen (with State)
+// ========================================
+class TodoApp extends StatefulWidget {
+  const TodoApp({super.key});rr
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TodoApp> createState() => _TodoAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TodoAppState extends State<TodoApp> {
+  // List of tarrsks - this is our State
+  final List<Todo> _todos = [];
 
-  void _incrementCounter() {
+  // Controller for the text field in dialog
+  final TextEditingController _textController = TextEditingController();
+
+  // ========================================
+  // Functions for managing tasks
+  // ========================================
+
+  // Add a new task
+  void _addTodo() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Add New Task'),
+          content: TextField(
+            controller: _textController,
+            decoration: const InputDecoration(
+              hintText: 'Enter task...',
+            ),
+            autofocus: true,
+          ),
+          actions: [
+            // Cancel button
+            TextButton(
+              onPressed: () {
+                _textController.clear();
+                Navigator.pop(context);
+              },
+              child: const Text('Cancel'),
+            ),
+            // Add button
+            TextButton(
+              onPressed: () {
+                if (_textController.text.isNotEmpty) {
+                  setState(() {
+                    _todos.add(Todo(title: _textController.text));
+                  });
+                  _textController.clear();
+                  Navigator.pop(context);
+                }
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Toggle task status (completed / not completed)
+  void _toggleTodo(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _todos[index].isDone = !_todos[index].isDone;
     });
   }
 
+  // Delete a task
+  void _deleteTodo(int index) {
+    setState(() {
+      _todos.removeAt(index);
+    });
+  }
+
+  // ========================================
+  // Building the UI
+  // ========================================
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      // AppBar - Top bar
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+        title: const Text('My Todos'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+
+      // Body - Screen content
+      body: _todos.isEmpty
+          // If no tasks - show message
+          ? const Center(
+              child: Text(
+                'No tasks yet!\nTap + to add one',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18, color: Colors.grey),
+              ),
+            )
+          // If there are tasks - show list
+          : ListView.builder(
+              itemCount: _todos.length,
+              itemBuilder: (context, index) {
+                final todo = _todos[index];
+                return ListTile(
+                  // Checkbox on the left
+                  leading: Checkbox(
+                    value: todo.isDone,
+                    onChanged: (_) => _toggleTodo(index),
+                  ),
+                  // Task text
+                  title: Text(
+                    todo.title,
+                    style: TextStyle(
+                      // Strikethrough if completed
+                      decoration:
+                          todo.isDone ? TextDecoration.lineThrough : null,
+                      // Gray color if completed
+                      color: todo.isDone ? Colors.grey : null,
+                    ),
+                  ),
+                  // Delete button on the right
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete, color: Colors.red),
+                    onPressed: () => _deleteTodo(index),
+                  ),
+                );
+              },
             ),
-          ],
-        ),
-      ),
+
+      // Floating button to add task
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: _addTodo,
+        tooltip: 'Add Task',
         child: const Icon(Icons.add),
       ),
     );
